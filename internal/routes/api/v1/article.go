00ff0225f3a25361df0bbe19acad4d46ca7139b5
Jerry YT Chen <nmjk2000@gmail.com>
2021-03-13 18:44:34 +0800

    feat: Response

--- internal/routes/api/v1/article.go
+++ internal/routes/api/v1/article.go
@@ -1,6 +1,10 @@
 package v1
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+	"github/ChenYuTingJerry/blog-service/pkg/app"
+	"github/ChenYuTingJerry/blog-service/pkg/errcode"
+)
 
 type Article struct{}
 
@@ -9,7 +13,10 @@ func NewArticle() Article {
 }
 
 func (t Article) Get(c *gin.Context)    {}
-func (t Article) List(c *gin.Context)   {}
+func (t Article) List(c *gin.Context)   {
+	app.NewResponse(c).ToErrorResponse(errcode.ServerError)
+	return
+}
 func (t Article) Create(c *gin.Context) {}
 func (t Article) Update(c *gin.Context) {}
 func (t Article) Delete(c *gin.Context) {}
